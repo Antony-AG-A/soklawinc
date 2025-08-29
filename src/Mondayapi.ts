e947ab82124ca8922033fffa199effc4622da5b3
Antony-AG-A <antonygitau205@gmail.com>
2025-08-28 22:32:35 -0400

    Updated Mondayapi.ts

--- src/Mondayapi.ts
+++ src/Mondayapi.ts
@@ -1,59 +1,47 @@
+// src/Mondayapi.ts
+import axios from "axios";
+
 const MONDAY_API_URL = "https://api.monday.com/v2";
-const MONDAY_API_KEY = import.meta.env.VITE_MONDAY_API_KEY as string;
+const MONDAY_API_KEY = import.meta.env.VITE_MONDAY_API_KEY;
 
-async function gql(query: string, variables: Record<string, any> = {}) {
-  const res = await fetch(MONDAY_API_URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization": MONDAY_API_KEY,
-    },
-    body: JSON.stringify({ query, variables }),
-  });
+const client = axios.create({
+  baseURL: MONDAY_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: MONDAY_API_KEY,
+  },
+});
 
-  const data = await res.json();
-
-  if (data.errors) {
-    console.error("GraphQL Errors:", data.errors);
-    throw new Error(`Monday API error: ${data.errors[0].message}`);
-  }
-
-  return data.data;
-}
-
-export async function getBoardColumns() {
+export async function getBoardColumns(boardId: string) {
   const query = `
-    query ($boardId: [ID!]!) {
-      boards (ids: $boardId) {
+    query {
+      boards(ids: ${boardId}) {
         columns {
           id
           title
           type
-          settings_str
         }
       }
     }
   `;
-
-  const data = await gql(query, { boardId: import.meta.env.VITE_MONDAY_BOARD_ID });
-  return data.boards[0].columns;
+  const response = await client.post("", { query });
+  return response.data.data.boards[0].columns;
 }
 
-export async function createBoardItem(itemName: string, columnValues: Record<string, any>) {
-  const query = `
-    mutation ($boardId: ID!, $itemName: String!, $columnValues: JSON!) {
-      create_item (board_id: $boardId, item_name: $itemName, column_values: $columnValues) {
+export async function createItem(boardId: string, itemName: string, columnValues: any) {
+  const mutation = `
+    mutation ($boardId: Int!, $itemName: String!, $columnValues: JSON!) {
+      create_item(board_id: $boardId, item_name: $itemName, column_values: $columnValues) {
         id
       }
     }
   `;
-
   const variables = {
-    boardId: import.meta.env.VITE_MONDAY_BOARD_ID,
+    boardId: parseInt(boardId, 10),
     itemName,
     columnValues: JSON.stringify(columnValues),
   };
 
-  const data = await gql(query, variables);
-  return data.create_item;
+  const response = await client.post("", { query: mutation, variables });
+  return response.data.data.create_item;
 }
