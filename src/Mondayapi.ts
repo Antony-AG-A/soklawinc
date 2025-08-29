f6ac98673b219492cc89da3b1ca3811b07719e56
Antony-AG-A <antonygitau205@gmail.com>
2025-08-28 23:21:55 -0400

    Updated Mondayapi.ts

--- src/Mondayapi.ts
+++ src/Mondayapi.ts
@@ -1,14 +1,10 @@
 const BOARD_ID = import.meta.env.VITE_MONDAY_BOARD_ID as string;
-
-// Point to your proxy, not Monday directly
 const MONDAY_PROXY_URL = "http://localhost:4000/monday";
 
 async function gql(query: string, variables: Record<string, any> = {}) {
   const res = await fetch(MONDAY_PROXY_URL, {
     method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ query, variables }),
   });
 
@@ -20,39 +16,4 @@ async function gql(query: string, variables: Record<string, any> = {}) {
   }
 
   return data.data;
-}
-
-export async function getBoardColumns() {
-  const query = `
-    query ($boardId: [ID!]!) {
-      boards (ids: $boardId) {
-        columns {
-          id
-          title
-          type
-          settings_str
-        }
-      }
-    }
-  `;
-  const data = await gql(query, { boardId: BOARD_ID });
-  return data.boards[0].columns;
-}
-
-export async function createBoardItem(itemName: string, columnValues: Record<string, any>) {
-  const query = `
-    mutation ($boardId: ID!, $itemName: String!, $columnValues: JSON!) {
-      create_item (board_id: $boardId, item_name: $itemName, column_values: $columnValues) {
-        id
-      }
-    }
-  `;
-  const variables = {
-    boardId: BOARD_ID,
-    itemName,
-    columnValues: JSON.stringify(columnValues),
-  };
-
-  const data = await gql(query, variables);
-  return data.create_item;
 }
